1fe5b2e6ae44ad9fe7a2142c8f15bae31ab55661
yatharth-vataliya <yatharthvataliya@gmail.com>
2023-12-28 20:15:29 +0530

    Using best and server efficient approach for uploading file chunks

--- resources/views/livewire/components/chunked-file-upload.blade.php
+++ resources/views/livewire/components/chunked-file-upload.blade.php
@@ -21,10 +21,6 @@
                 <div class="p-2" x-show="progressValueInPercentage[index]">
                     <span x-text="`Total progress in percentage :- ${progressValueInPercentage[index]} %`"></span>
                 </div>
-                <div class="p-2" x-show="chunkUploadProgressValue[index]">
-                    <progress class="w-full" max="100" x-bind:value="chunkUploadProgressValue[index]"
-                        x-bind:id="`fileUploadChunkProgress_${index}`"></progress>
-                </div>
                 <div class="p-2 w-full" x-show="progressValue[index]">
                     <progress class="w-full" x-bind:max="maxProgressValue[index]" x-bind:value="progressValue[index]"
                         x-bind:id="`fileUploadProgress_${index}`"></progress>
@@ -44,7 +40,6 @@
                     progressValue: [],
                     chunkSize: 1024 * 1024 * 5,
                     progressValueInPercentage: [],
-                    chunkUploadProgressValue: [],
                     uploadChange(el) {
                         this.isButtonDisabled = el.target.files.length > 0 ? false : true;
                         this.files = [...document.querySelector('#upload').files];
@@ -53,12 +48,10 @@
                             this.progressValue[index] = 0;
                             this.progressValueInPercentage[index] = 0;
                             this.maxProgressValue[index] = 100;
-                            this.chunkUploadProgressValue[index] = 0;
                         });
                     },
                     async uploadChunks() {
                         this.isButtonDisabled = true;
-                        console.log(this.files.length);
                         for (let i = 0; i < this.files.length; i++) {
                             const index = i;
                             const tempFile = this.files[index];
@@ -66,91 +59,41 @@
                             const tempFileSize = this.files[index].size;
                             this.maxProgressValue[index] = Math.round(tempFileSize > this.chunkSize ? tempFileSize /
                                 this.chunkSize : 100);
-                            @this.set('fileName', tempFileName);
-                            @this.set('isFirstCall', true);
                             this.fileName = tempFileName;
-                            await this.livewireUploadChunk(tempFile, 0, index);
-                            if (index == (1 - this.files.length)) {
+                            await this.uploadFileChunk(tempFile, 0, index);
+                            if (index == (this.files.length - 1)) {
+                                this.isFirstCall = [];
                                 this.isButtonDisabled = false;
                                 this.$refs.upload.value = '';
                                 this.$refs.upload.dispatchEvent(new Event('change'));
                             }
                         }
-                        /*await this.files.forEach(async (file, index) => {
-                            this.maxProgressValue[index] = Math.round(file.size > this.chunkSize ? file
-                                .size / this
-                                .chunkSize :
-                                100);
-                            @this.set('fileName', file.name);
-                            @this.set('isFirstCall', true);
-                            this.fileName = file.name;
-                            await this.livewireUploadChunk(file, 0, index);
-                            if (index == this.files.length) {
-                                this.isButtonDisabled = false;
-                                this.$refs.upload.value = '';
-                                this.$refs.upload.dispatchEvent(new Event('change'));
-                            }
-                        });*/
-                        //this.isButtonDisabled = false;
-                        //this.$refs.upload.value = '';
-                        //this.$refs.upload.dispatchEvent(new Event('change'));
                     },
-                    async livewireUploadChunk(file, start, index) {
+                    async uploadFileChunk(file, start, index) {
 
-                        //This method to submit files is using Livewire techniques.
-                        const chunkEnd = Math.min(start + this.chunkSize, file.size);
-                        const chunk = file.slice(start, chunkEnd);
-                        await @this.upload('fileChunk', chunk,
-                            async (uploadedFileName) => {
-                                    console.info('This is file name that uploaded recenctly ',
-                                        uploadedFileName);
-                                    delete chunk;
-                                    @this.set('isFirstCall', false);
-                                    this.progressValue[index] = ++this.progressValue[index];
-                                    if (this.progressValue[index] > 0 && file.size > this.chunkSize) {
-                                        this.progressValueInPercentage[index] = Math.round((this
-                                                .progressValue[
-                                                    index] * 100) /
-                                            totalChunks);
-                                    } else {
-                                        this.progressValueInPercentage[index] = 100;
-                                    }
-                                },
-                                () => {
-                                    console.error('This is error while uploading file');
-                                },
-                                async (event) => {
-                                    this.chunkUploadProgressValue[index] = event.detail.progress;
-                                    if (event.detail.progress == 100) {
-                                        if (chunkEnd < file.size) {
-                                            await this.livewireUploadChunk(file, chunkEnd, index);
-                                        }
-                                    }
-                                    delete chunkEnd;
-                                }
-                        );
-
-                        // This method to submit files is using fetch API and for loop.
-                        /*const formData = new FormData();
+                        const formData = new FormData();
                         formData.set('_token', '{{ csrf_token() }}');
-                        formData.set('fileName', this.fileName);
-                        formData.set('isFirstCall', this.isFirstCall);
+                        formData.set('fileName', file.name);
+                        formData.set('isFirstCall', true);
+                        const totalChunks = Math.ceil(file.size / this.chunkSize);
+                        this.maxProgressValue[index] = totalChunks;
                         if (file.size < this.chunkSize) {
                             formData.set('fileData', file);
                             await fetch('{{ route('upload-data') }}', {
                                 method: 'POST',
                                 body: formData,
                             }).then(async (response) => {
-                                delete formData;
+                                if (await response.ok) {
+                                    this.progressValue[index] = 100;
+                                    this.progressValueInPercentage[index] = 100;
+                                }
                             });
-                            this.isFirstCall = true;
                         }
                         if (file.size > this.chunkSize) {
                             for (let i = 0; i < file.size; i = i + this.chunkSize) {
                                 let chunk;
                                 if (i !== 0) {
                                     formData.set('isFirstCall', false);
-                                    this.isFirstCall = false;
                                 }
                                 chunk = file.slice(i, (i + this.chunkSize));
                                 formData.set('fileData', chunk);
@@ -158,50 +101,17 @@
                                     method: 'POST',
                                     body: formData,
                                 }).then(async (response) => {
-                                    console.log('this is line number 82 without in condition');
-                                    console.log(await response.text());
+                                    if (await response.ok) {
+                                        this.progressValue[index] += 1;
+                                        this.progressValueInPercentage[index] = Math.round((100 * this
+                                            .progressValue[index]) / totalChunks);
+                                    }
                                 });
+                                delete chunk;
                             }
                         }
-                        //const response = await Promise.allSettled(uploadChunksArray); // this is not working because it doesn't respect the order of request
-
-                        this.isFirstCall = true;
-                        this.isButtonDisabled = false;
-                        this.$refs.upload.value = '';
-                        this.$refs.upload.dispatchEvent(new Event('change'));
-                        */
-
-                        // This method to submit files is using fetch API and recursion of function.
-                        /*const chunkEnd = Math.min(start + this.chunkSize, file.size);
-                        const chunk = file.slice(start, chunkEnd);
-                        const formData = new FormData();
-                        const totalChunks = file.size > this.chunkSize ? Math.ceil(file.size / this.chunkSize) : 1;
-                        formData.set('fileData', chunk);
-                        formData.set('_token', '{{ csrf_token() }}');
-                        formData.set('fileName', file.name);
-                        formData.set('isFirstCall', this.isFirstCall[index]);
-                        await fetch('{{ route('upload-data') }}', {
-                            method: 'POST',
-                            body: formData,
-                        }).then(async (response) => {
-                            delete formData;
-                            this.isFirstCall[index] = false;
-                            this.progressValue[index] = ++this.progressValue[index];
-                            if (this.progressValue[index] > 0 && file.size > this.chunkSize) {
-                                this.progressValueInPercentage[index] = Math.round((this.progressValue[
-                                        index] * 100) /
-                                    totalChunks);
-                            } else {
-                                this.progressValueInPercentage[index] = 100;
-                            }
-                            start = chunkEnd;
-                            if (start < file.size) {
-                                await this.livewireUploadChunk(file, start, index);
-                            }
-                        });
-                        this.progressValue[index] = this.maxProgressValue[index];
-                        this.progressValue[index] = 0;
-                        this.isFirstCall[index] = true;*/
+                        delete formData;
+                        delete totalChunks;
                     },
                 };
             });
