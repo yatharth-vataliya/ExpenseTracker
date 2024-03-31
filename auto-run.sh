75af999fd4a6d6495e3b77b3b80d417b2bfbdfa4
yatharth-vataliya <yatharthvataliya@gmail.com>
2024-03-31 11:10:45 +0530

    changes done for deployment

---
+++ auto-run.sh
@@ -0,0 +1,17 @@
+git pull;
+
+composer install --optimize-autoloader --no-dev
+
+php artisan migrate;
+
+php artisan config:cache;
+
+php artisan event:cache;
+
+php artisan route:cache;
+
+php artisan view:cache;
+
+npm install;
+
+npm run dev;
