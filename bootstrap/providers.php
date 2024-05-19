66d3020a8c85823a253d4eb593f3dfe96d66f53e
yatharth-vataliya <yatharthvataliya@gmail.com>
2024-05-19 19:19:57 +0530

    upgrade to laravel 11.x is done

---
+++ bootstrap/providers.php
@@ -0,0 +1,9 @@
+<?php
+
+
+return [
+    App\Providers\AppServiceProvider::class,
+    App\Providers\CustomDirectiveServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
+    App\Providers\VoltServiceProvider::class,
+];
