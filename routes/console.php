2993c791dfddaced870e1059eea807805a3cb5f9
yatharth vataliya <yatharthvataliya@gmail.com>
2024-09-22 16:55:09 +0530

    now when user is working in local environment and if os environment is perfect for fetching battery status then it will prompt 80 percent battery charged when it will charged 80 % (#9)

--- routes/console.php
+++ routes/console.php
@@ -2,6 +2,9 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Process;
+use Illuminate\Support\Facades\Schedule;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +20,32 @@ use Illuminate\Support\Facades\Artisan;
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Artisan::command('battery-alert', function () {
+    $result = Process::pipe([
+        'upower --dump',
+        'grep percentage',
+        'head -n 1',
+    ]);
+    if ($result->successful()) {
+        $rawOutput = $result->output();
+        $batteryPercentage = explode(':', $rawOutput)[1];
+        $batteryPercentage = (int) trim(trim($batteryPercentage), '%');
+        if ($batteryPercentage === 80) {
+            $env = config('app.env');
+            if ($env === 'local') {
+                Process::path(public_path('/CustomAssests/images/'))
+                    ->command('export DISPLAY=:0.0 && xdg-open battery-80-percent-alert.png')
+                    ->run();
+
+            }
+        }
+    }
+    if ($result->failed()) {
+        Log::channel('daily')->error(
+            'Time :- '.now(tz: config('app.timezone_indian').' There is a problem running `battery-alert` command and the issue is '.$result->errorOutput())
+        );
+    }
+})->purpose('Alert user if batter is 80% charged.');
+
+Schedule::command('battery-alert')->everyMinute()->timezone(config('app.timezone_indian'));
